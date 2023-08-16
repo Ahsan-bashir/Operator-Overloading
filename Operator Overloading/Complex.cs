cd83a219d1a92e0b39657208be2604ad4f9443d2
Ahsan-bashir <bteeccc@gmail.com>
2023-08-16 12:29:49 +0500

    OOP Concepts Uploaded

---
+++ Operator Overloading/Complex.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operator_Overloading
+{
+    class Complex
+    {
+        private int real,imaginary;
+        public Complex()
+        {
+                
+        }
+        public Complex(int real,int imaginary)
+        {
+            this.real = real;
+            this.imaginary = imaginary;
+        }
+        public static Complex operator + (Complex c1, Complex c2)
+        {
+
+            return new Complex(c1.real + c2.real, c1.imaginary + c2.imaginary);
+            //Complex temp = new Complex();
+            //temp.real = c1.real + c2.real;
+            //temp.imaginary = c1.imaginary + c2.imaginary;
+            //return temp;
+        }
+        public static Complex operator +(Complex c1, int c2)
+        {
+
+            return new Complex(c1.real + c2, c1.imaginary + c2);
+            //Complex temp = new Complex();
+            //temp.real = c1.real + c2.real;
+            //temp.imaginary = c1.imaginary + c2.imaginary;
+            //return temp;
+        }
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+        public static Complex operator -(Complex c1, Complex c2)
+        {
+            return new Complex(c1.real - c2.real, c1.imaginary - c2.imaginary);
+
+            //Complex temp = new Complex();
+            //temp.real = c1.real - c2.real;
+            //temp.imaginary = c1.imaginary - c2.imaginary;
+            //return temp;
+        }
+        public static bool operator <(Complex c1, Complex c2)
+        {
+          if(c1.real<c2.real  && c1.imaginary < c2.imaginary)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator >(Complex c1, Complex c2)
+        {
+            if (c1.real > c2.real && c1.imaginary >c2.imaginary)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator ==(Complex c1,Complex c2)
+        {
+            if(c1.real==c2.real && c1.imaginary == c2.imaginary)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator!=(Complex c1, Complex c2)
+        {
+            if (c1.real != c2.real && c1.imaginary != c2.imaginary)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public override string ToString()
+        {
+            return string.Format("{0} \t {1} ",real,imaginary);
+        }
+    }
+}
---
+++ Operator Overloading/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Operator_Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Complex firstObj = new Complex(60, 30);
+            Complex secondObj = new Complex(60, 30);
+            //Console.WriteLine(firstObj.Equals(secondObj));
+            Complex addResult = firstObj + secondObj;
+            Complex minusResult = firstObj - secondObj;
+            Console.WriteLine("Real \t Imaginary");
+            Console.WriteLine(addResult);
+            Console.WriteLine(minusResult);
+            if (firstObj < secondObj)
+            {
+                Console.WriteLine("First object is smaller !!!");
+            }
+            else
+            {
+                Console.WriteLine("First  object is Greater!!");
+            }
+            int num1 = 2, num2 = 6;
+            int num = num1 + num2;
+            Console.WriteLine(firstObj += 1);
+        }
+        
+
+    }
+}
